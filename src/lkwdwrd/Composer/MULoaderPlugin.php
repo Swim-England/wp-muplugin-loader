3938b7c0b41e64669b744061fb2ad8554c46d776
Graham Spratt <graham.spratt@swimming.org>
2024-12-02 22:58:10 +0000

    Update MULoaderPlugin.php

--- src/lkwdwrd/Composer/MULoaderPlugin.php
+++ src/lkwdwrd/Composer/MULoaderPlugin.php
@@ -212,4 +212,15 @@ class MULoaderPlugin implements PluginInterface, EventSubscriberInterface {
 
 		return $separator;
 	}
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement deactivate() method.
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement uninstall() method.
+    }
+
 }
